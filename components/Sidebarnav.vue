f34f5925335bd978cec52ae52e0503e5a3fbb887
aryawiratama <aryawiratama99@gmail.com>
2019-01-18 04:00:52 +0800

    setup grid

--- components/Sidebarnav.vue
+++ components/Sidebarnav.vue
@@ -2,8 +2,14 @@
   <nav class="sidebar-nav">
     <h2 class="sidebar-title">Quiz Section</h2>
     <ul class="nav">
+      <li class="nav-item active">
+        <a class="nav-link">Section 1</a>
+      </li>
       <li class="nav-item">
-        <a class="nav-link active">Section 1</a>
+        <a class="nav-link">Section 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">Section 3</a>
       </li>
     </ul>
   </nav>
--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,18 @@
     <headernav/>
     <div class="container-fluid page-body-wrapper">
       <sidebarnav/>
+      
       <div class="main-panel">
-        Content
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-6 grid-margin"><div class="card"><div class="card-body"><h2>Content</h2></div></div></div>
+            <div class="col-6 grid-margin"><div class="card"><div class="card-body"><h2>Content</h2></div></div></div>
+            <div class="col-6 grid-margin"><div class="card"><div class="card-body"><h2>Content</h2></div></div></div>
+            <div class="col-6 grid-margin"><div class="card"><div class="card-body"><h2>Content</h2></div></div></div>
+          </div>
+        </div>
       </div>
+
     </div>
   </div>
 </template>
